6a44531df076e5d1bfeb14aa4d3309b6d5abf265
Francis Mendez <fmendez884@gmail.com>
2019-04-23 15:47:47 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -31,7 +31,7 @@ def bulk_tweet_shortener(tweets)
 end 
 
 def selective_tweet_shortener(tweet)
-  if tweet.length < 140
+  if tweet.length > 140
     puts word_substituter(tweet)
   else 
    puts tweet
