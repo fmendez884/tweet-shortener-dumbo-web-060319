10f1c811ee3cdd06526d70d0ce772364e405407b
Francis Mendez <fmendez884@gmail.com>
2019-04-23 10:26:58 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,39 @@
 # Write your code here.
+def dictionary 
+hash = {
+    "Hello" => "hi",
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@",
+    "and" => "&"
+}
+end
+
+def word_substituter(tweet)
+  shortened = tweet.split.collect do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else word
+    end
+  end
+  shortened.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |x|
+    puts word_substituter(x)
+  end
+end 
+
+def selective_tweet_shortener(tweet)
+  if tweet.size > 140
+    puts word_substituter(tweet)
+  else 
+   puts tweet
+  end
+end
